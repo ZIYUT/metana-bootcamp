7c0fd9005a6080cee4d2ae73b33fc2f11e26f665
ZIYUT <tianziyu98@gmail.com>
2025-02-08 16:43:46 +0800

    M2

--- module-2 deliverables/ERC721_contract.sol
+++ module-2 deliverables/ERC721_contract.sol
@@ -2,6 +2,8 @@
 pragma solidity ^0.8.19;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract myNFT is ERC721URIStorage, Ownable {
@@ -15,5 +17,26 @@ contract myNFT is ERC721URIStorage, Ownable {
         _safeMint(recipient, itemID);
         _setTokenURI(itemID, tokenURI);
         return itemID;
+    }
+}
+contract myToken is ERC20, Ownable {
+    constructor() ERC20("myToken", "TKN") Ownable(msg.sender) {
+        _mint(msg.sender, 10 * 10**18);
+    }
+}
+contract NFTMinter is Ownable {
+    myToken public token;
+    myNFT public nft;
+    uint256 public constant TOKEN_COST = 10 * 10**18;
+
+    constructor(address ERC20Contract, address ERC721Contract)  Ownable(msg.sender) {
+        token = myToken(ERC20Contract);
+        nft = myNFT(ERC721Contract);
+    }
+    function mint(string memory tokenURI) external {
+        require(token.balanceOf(msg.sender) >= TOKEN_COST, "Insufficient token balance");
+        require(token.allowance(msg.sender, address(this)) >= TOKEN_COST, "Insufficient token allowance");
+        token.transferFrom(msg.sender, address(this), TOKEN_COST);
+        nft.mintNFT(msg.sender, tokenURI);
     }
 }

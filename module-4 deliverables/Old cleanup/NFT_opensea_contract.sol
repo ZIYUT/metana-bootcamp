fb0c3a6f5c8c2c67c428a8a2f4f1ef2f15909c3c
ZIYU TIAN <ziyut@student.unimelb.edu.au>
2025-02-23 23:50:03 +0800

    Run slither for the contracts

--- module-4 deliverables/Old cleanup/NFT_opensea_contract.sol
+++ module-4 deliverables/Old cleanup/NFT_opensea_contract.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.20; // 更新 Solidity 版本为 ^0.8.20
 
-import "@openzeppelin/contracts/utils/Strings.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import '@openzeppelin/contracts/utils/Strings.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
+import '@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol';
 
 contract MyNFT is ERC721URIStorage, Ownable {
   uint256 private tokenIds = 0;
@@ -12,17 +12,20 @@ contract MyNFT is ERC721URIStorage, Ownable {
 
   constructor(
     string memory baseURI
-  ) ERC721("myNFT", "NFT") Ownable(msg.sender) {
+  ) ERC721('myNFT', 'NFT') Ownable(msg.sender) {
     baseTokenURI = baseURI;
   }
 
+  // 修正mintNFT函数，确保先更新状态再调用外部函数，防止重入攻击
   function mintNFT(
     address recipient,
-    string memory tokenURI
+    string memory newTokenURI
   ) external onlyOwner {
-    require(tokenIds < MAX_SUPPLY, "No more NFTs can be minted.");
+    require(tokenIds < MAX_SUPPLY, 'No more NFTs can be minted.');
     tokenIds++;
+
+    // 先更新状态再调用外部函数
     _safeMint(recipient, tokenIds);
-    _setTokenURI(tokenIds, tokenURI);
+    _setTokenURI(tokenIds, newTokenURI); // 改名以避免与 ERC721 的 tokenURI 重名
   }
 }
--- module-4 deliverables/Old cleanup/NFT_staking_contract.sol
+++ module-4 deliverables/Old cleanup/NFT_staking_contract.sol
@@ -1,15 +1,16 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+import '@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol';
+import '@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
 
 contract MyToken is ERC20, Ownable {
-  constructor() ERC20("myToken", "TKN") Ownable(msg.sender) {
+  constructor() ERC20('myToken', 'TKN') Ownable(msg.sender) {
     _mint(msg.sender, 10 * 10 ** 18);
   }
+
   function mint(address add, uint256 amount) external onlyOwner {
     _mint(add, amount);
   }
@@ -18,7 +19,7 @@ contract MyToken is ERC20, Ownable {
 contract MyNFT is ERC721URIStorage, Ownable {
   uint256 private tokenID = 0;
 
-  constructor() ERC721("myNFT", "NFT") Ownable(msg.sender) {}
+  constructor() ERC721('myNFT', 'NFT') Ownable(msg.sender) {}
 
   function mintNFT(
     address recipient,
@@ -33,8 +34,8 @@ contract MyNFT is ERC721URIStorage, Ownable {
 }
 
 contract NFTStaker is Ownable, IERC721Receiver {
-  myToken public token;
-  myNFT public nft;
+  MyToken public immutable token;
+  MyNFT public immutable nft;
   mapping(uint256 => address) public originalOwner;
   mapping(address => uint256) public timestamps;
   uint256 public constant REWARD_RATE = 10 * 10 ** 18;
@@ -46,37 +47,48 @@ contract NFTStaker is Ownable, IERC721Receiver {
   }
 
   constructor(address TOKEN, address NFT) Ownable(msg.sender) {
-    token = myToken(TOKEN);
-    nft = myNFT(NFT);
+    token = MyToken(TOKEN);
+    nft = MyNFT(NFT);
   }
 
+  // 修改withdrawNFT函数，遵循Checks-Effects-Interactions模式
+  function withdrawNFT(uint256 tokenID) external {
+    require(originalOwner[tokenID] == msg.sender, 'Not the original owner');
+
+    // 先更新状态变量
+    delete originalOwner[tokenID];
+    delete timestamps[msg.sender];
+
+    // 再进行外部调用
+    nft.safeTransferFrom(address(this), msg.sender, tokenID);
+  }
+
+  // 修改depositNFT函数，遵循Checks-Effects-Interactions模式
   function depositNFT(uint256 tokenID) external {
-    require(nft.ownerOf(tokenID) == msg.sender, "You are not the NFT owner");
+    require(nft.ownerOf(tokenID) == msg.sender, 'You are not the NFT owner');
     require(
       nft.getApproved(tokenID) == address(this) ||
         nft.isApprovedForAll(msg.sender, address(this)),
-      "Contract not approved"
+      'Contract not approved'
     );
 
+    // 先更新状态变量
     originalOwner[tokenID] = msg.sender;
     timestamps[msg.sender] = block.timestamp;
+
+    // 再进行外部调用
     nft.safeTransferFrom(msg.sender, address(this), tokenID);
   }
 
-  function withdrawNFT(uint256 tokenID) external {
-    require(originalOwner[tokenID] == msg.sender, "Not the original owner");
-    nft.safeTransferFrom(address(this), msg.sender, tokenID);
-    delete originalOwner[tokenID];
-    delete timestamps[msg.sender];
-  }
-
+  // 修改rewards函数，修复乘法除法顺序
   function rewards() external {
     uint256 timePassed = block.timestamp - timestamps[msg.sender];
     require(
       timePassed >= STAKE_DURATION,
-      "Not long enough to receive the rewards"
+      'Not long enough to receive the rewards'
     );
-    uint256 rewardToken = (timePassed / STAKE_DURATION) * REWARD_RATE;
+
+    uint256 rewardToken = (timePassed * REWARD_RATE) / STAKE_DURATION;
     timestamps[msg.sender] = block.timestamp;
     token.mint(msg.sender, rewardToken);
   }

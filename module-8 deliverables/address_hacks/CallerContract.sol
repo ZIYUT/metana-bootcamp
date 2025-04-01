b806e6e3a551bffca4eddaba32eca50f5e5811f6
ZIYUT <tianziyu98@gmail.com>
2025-04-01 23:56:24 +1100

    Update CallerContract.sol

--- module-8 deliverables/address_hacks/CallerContract.sol
+++ module-8 deliverables/address_hacks/CallerContract.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/utils/Address.sol";
 import "./RestrictedContract.sol";
 
-// CallerContract: A contract that demonstrates extcodesize bypass and call restrictions
 contract CallerContract {
     using Address for address;
 
@@ -16,39 +15,31 @@ contract CallerContract {
     event ConstructorSelfCall(bool success);
     event ConstructorExtCodeSize(uint256 size);
 
-    // Constructor: Takes the address of RestrictedContract and performs calls
     constructor(address restricted) {
-        // Get the extcodesize of this contract
         uint256 size;
         assembly {
             size := extcodesize(address())
         }
         constructorExtCodeSize = size;
         emit ConstructorExtCodeSize(size);
-
-        // Attempt to call onlyEOA function of RestrictedContract
         (bool successRestricted, ) = address(restricted).call(
             abi.encodeWithSelector(RestrictedContract.onlyEOA.selector)
         );
         constructorCallToRestrictedSucceeded = successRestricted;
         emit ConstructorCallToRestricted(successRestricted);
 
-        // Attempt to call the dummy function of this contract
-        // Should fail because extcodesize is 0 during construction
         if (size == 0) {
-            // If extcodesize is 0, expect the call to fail
             (bool successSelf, ) = address(this).call(
                 abi.encodeWithSelector(this.nothing.selector)
             );
             constructorSelfCallSucceeded = successSelf;
             emit ConstructorSelfCall(successSelf);
-            // Additional check: If successSelf is true despite size == 0, log an anomaly
             if (successSelf) {
-                emit ConstructorSelfCall(false); // Force failure indication
-                constructorSelfCallSucceeded = false; // Override to false
+                emit ConstructorSelfCall(false); 
+                constructorSelfCallSucceeded = false;
             }
         } else {
-            constructorSelfCallSucceeded = false; // Should not happen
+            constructorSelfCallSucceeded = false;
             emit ConstructorSelfCall(false);
         }
     }
@@ -56,14 +47,11 @@ contract CallerContract {
     function nothing() external {
 
     }
-
-    // Check self-call: Attempts to call dummy after deployment
     function checkSelfCall() external {
         address(this).functionCall(abi.encodeWithSelector(this.nothing.selector));
         // If no revert occurs, the call was successful
     }
 
-    // Get extcodesize: Returns the current extcodesize of this contract
     function getExtCodeSize() external view returns (uint256) {
         uint256 size;
         assembly {
